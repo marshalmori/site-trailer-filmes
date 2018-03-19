07f72f7574d8b7d430882cfe7a05c1692271c6e6
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2018-03-19 09:32:38 -0300

    Criação da estrutura de dados, título, URL do cartaz, link YouTube do trailer

---
+++ entertainment.py
@@ -0,0 +1,8 @@
+import media
+
+forma = media.Movie("Forma d'água.",
+                    "http://t2.gstatic.com/images?q=tbn:ANd9GcSsVWxdLjAFyWj0YtR6cHiB8qfNl96yfC5O5QHw8Y4Cupp4fK_5",
+                    "https://www.youtube.com/watch?v=XFYWazblaUA")
+
+print(forma.title)
+forma.show_trailer()
